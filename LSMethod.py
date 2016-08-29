efe3847ddaa2cb22e5f149d8254c872ec297a57e
Nontawat Numor <nontawat39@gmail.com>
2016-08-29 16:15:45 +0700

    plot data to show

--- LSMethod.py
+++ LSMethod.py
@@ -5,6 +5,24 @@ import matplotlib.pyplot as plt
 dataX = []
 dataY = []
 
+
+def multiplyList(dataList, number):
+    newList = []
+
+    for item in dataList:
+        newList.append(float(item)*number)
+
+    return newList
+
+
+def addList(dataList, number):
+    newList = []
+
+    for item in dataList:
+        newList.append(float(item)+number)
+
+    return newList
+
 with open('LS.csv', 'rb') as fileLS:
     reader = csv.reader(fileLS)
 
@@ -26,3 +44,15 @@ A = np.vstack([x, np.ones(len(x))]).T
 m, c = np.linalg.lstsq(A, y)[0]
 
 # print (m, c)
+
+equation = x
+equation = multiplyList(equation, m)
+equation = addList(equation, c)
+
+print "Equation have m = %s and c = %s" % (m, c)
+print "Formula is y = %sx + %s" % (m, c)
+
+plt.plot(x, y, 'o', label='Raw data', markersize=3)
+plt.plot(x, equation, 'r', label='Fitted line')
+plt.legend()
+plt.show()
