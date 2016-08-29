03ea4877ff17a3c687f7a3900accf488dd2a88a8
Nontawat Numor <nontawat39@gmail.com>
2016-08-29 15:51:18 +0700

    insert data to list

--- LSMethod.py
+++ LSMethod.py
@@ -1,11 +1,16 @@
 import csv
 
+dataX = []
+dataY = []
+
 with open('LS.csv', 'rb') as fileLS:
     reader = csv.reader(fileLS)
 
     thisData = False
     for row in reader:
         if thisData:
-            print "%s %s " % (row[0], row[1])
+            dataX.append(row[0])
+            dataY.append(row[1])
+            # print "%s %s " % (row[0], row[1])
 
         thisData = True
