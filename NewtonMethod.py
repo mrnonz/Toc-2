2cfae066212e356801e99a897ffe6358b39e5e7a
Nontawat Numor <nontawat39@gmail.com>
2016-08-29 21:56:04 +0700

    fix bug

--- NewtonMethod.py
+++ NewtonMethod.py
@@ -3,22 +3,27 @@ from decimal import *
 
 precision = 10**(-50)
 
+
 def average(x, y):
 	return (x + y) / 2.0
 
+
 def improve(guess, number):
 	return average(guess, number/guess)
+
 
 def good_enough(guess, number):
 	temp = guess**2
 	temp -= number
 	return abs(temp) < precision
 
+
 def sqrt_apox(guess, number):
 	while not good_enough(guess, number):
 		guess = improve(guess, number)
 
 	return guess
+
 
 def sqrtNewton(number):
 	return Decimal(sqrt_apox(1, number))
